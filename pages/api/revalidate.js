d54fe041b45e57cc16f9a8128dd2c802e7bed68b
Om Nikam <omnikam0404@gmail.com>
2022-07-09 00:07:31 +0530

    forced revalidate

---
+++ pages/api/revalidate.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res){
+    for(const url of req.body){
+        await res.unstable_revalidate(url)
+    }
+
+    res.status(200).json({revalidate: true})
+} 
