c9c1be65ff9dd62badff5e768d4024f6dba4528b
Om Nikam <omnikam0404@gmail.com>
2022-07-08 23:42:28 +0530

    static site generation SSG

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,23 +1,35 @@
-
 import React from "react";
 import Head from "next/head";
 import styles from "../../styles/Details.module.css";
 import Link from "next/link";
 
-export async function getServerSideProps({ params }){
+export async function getStaticPaths() {
    const resp = await fetch(
-      `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
+     "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
    );
-
+   const pokemon = await resp.json();
+ 
    return {
-      props:{
-         pokemon: await resp.json()
-      }
-   }
+     paths: pokemon.map((pokemon) => ({
+       params: { id: pokemon.id.toString() },
+     })),
+     fallback: false,
+   };
+ }
+
+export async function getStaticProps({ params }) {
+  const resp = await fetch(
+    `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
+  );
+
+  return {
+    props: {
+      pokemon: await resp.json(),
+    },
+  };
 }
 
-export default function Details({pokemon}) {
-
+export default function Details({ pokemon }) {
   if (!pokemon) {
     return null;
   }
@@ -25,7 +37,7 @@ export default function Details({pokemon}) {
   return (
     <div>
       <head>
-        <title>{pokemon.name.english}</title>
+        <title>{pokemon.name}</title>
       </head>
 
       <div>
@@ -48,24 +60,22 @@ export default function Details({pokemon}) {
           <div className={styles.type}>{pokemon.type}</div>
         </div>
         <div>
-         
-        <table>
-          <thead className={styles.header}>
-            <tr>
-              <th>Name</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            {pokemon.stats.map(({ name, value }) => (
-              <tr key={name}>
-                <td className={styles.attribute}>{name}</td>
-                <td>{value}</td>
+          <table>
+            <thead className={styles.header}>
+              <tr>
+                <th>Name</th>
+                <th>Value</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        
+            </thead>
+            <tbody>
+              {pokemon.stats.map(({ name, value }) => (
+                <tr key={name}>
+                  <td className={styles.attribute}>{name}</td>
+                  <td>{value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
